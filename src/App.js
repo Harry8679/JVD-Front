e03ec9c71c745593727e03003bd83214ffcf1c45
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-08-28 10:30:48 +0200

    App 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path='/' element={<} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
