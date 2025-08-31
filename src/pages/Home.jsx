48aeb36894677733780163aa80ea53b3aa58f4f5
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-08-31 09:13:04 +0200

    Home Page updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import Build from '../components/Build';
-import Short from '../components/Short';
+import React from "react";
+import Build from "../components/Build";
+import Short from "../components/Short";
 
 const Home = () => {
   return (
     <>
-      <Build />
-      <Short />
+      <Build
+        // si tu veux une vidéo 16:9 sous l’image héro :
+        // videoId="jNQXAC9IVRw"
+      />
+      <Short
+        videos={[
+          "hY7m5jjJ9mM", // <-- remplace par tes IDs YouTube
+          "dQw4w9WgXcQ",
+          "9bZkp7q19f0",
+        ]}
+        sideTitle="VOUS SOMI GROUNDBREAKING"
+        sideSubtitle="Le cœur derrière la vision"
+        sideText="Texte explicatif sur le projet, la vision et les prochaines étapes."
+        ctaLabel="See the vision"
+        ctaHref="/vision"
+      />
     </>
-  )
-}
+  );
+};
 
 export default Home;
