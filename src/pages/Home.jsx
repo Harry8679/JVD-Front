d520aed4ea07b330fd51ea20bda2d900af40ee46
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-09-01 18:58:59 +0200

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Build from "../components/Build";
+import LatestUpdateCard from "../components/LatestUpdateCard";
 import Short from "../components/Short";
 import SomiBuildingUpdate from "../components/SomiBuildingUpdate";
 
@@ -17,11 +18,27 @@ const Home = () => {
         ]}
         sideTitle="VOUS SOMI GROUNDBREAKING"
         sideSubtitle="Le cœur derrière la vision"
-        sideText="Texte explicatif sur le projet, la vision et les prochaines étapes."
+        sideText="Texte explicatif sur le projet, la vision et les prochaines étapes. Nous lançons une collecte de dons pour soutenir l’Église MLK dans sa mission.
+        Chaque contribution, petite ou grande, nous aide à bâtir et à faire grandir cette vision.
+        Ensemble, semons pour aujourd’hui et pour les générations à venir. Chers frères et sœurs,
+        Nous lançons une collecte de dons pour soutenir l’Église MLK dans sa mission et ses projets.
+        Chaque geste de générosité, petit ou grand, contribue à l’avancement de l’œuvre de Dieu.
+        Vos dons permettront de bâtir, d’équiper et de renforcer notre communauté.
+        Ensemble, nous portons une vision qui dépasse nos vies et bénira les générations futures.
+        Merci de participer avec foi et amour à cette belle aventure spirituelle."
         ctaLabel="See the vision"
         ctaHref="/vision"
       />
       <SomiBuildingUpdate raised={7724443} goal={32500000} footnoteGoal={16250000} />
+      <LatestUpdateCard
+        paragraphs={[
+        "Over the last year and a half, our building team has been working with the city and our architectural firm to develop the new VOUS South Miami building.",
+        "As of Monday, July 7th, we have officially signed our construction contract to begin the build-out of our brand-new VOUS South Miami facility.",
+        "We will officially break ground on Monday, August 4th. This is a huge milestone and the commencement of our building timeline, which is approximately 24 months.",
+        "As we begin construction, our SoMi Sunday location will be moving to South Miami Senior High starting Sunday, August 3rd. Same heart, same community, just a temporary new location as we prepare for all that's ahead.",
+        "We extend our heartfelt gratitude for the unwavering support we've received thus far. With faith and trust, we're fully committed to advancing this exciting project together. Thank you for your prayers and contributions thus far!",
+        ]}
+      />
     </>
   );
 };
